96b961ad44bf00310cd98426dbbeee7967223f90
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2026-03-28 14:03:00 +0200

    made also regression header for diamond inheritance

---
+++ include/Regressor.h
@@ -0,0 +1,21 @@
+#pragma once 
+#include "MLModel.h"
+
+class Regressor : virtual public MLModel{
+    double l2Penalty;
+
+public:
+    Regressor () : MLModel(), l2Penalty(0.0) {}
+    Regressor(std::string modelName, const Hyperparameters& hp, double l2 = 0.0)
+        : MLModel(modelName, hp), l2Penalty(l2) {}
+    
+    virtual ~Regressor() = default;
+
+    // methods
+    double getL2Penalty() const{
+        return l2Penalty;
+    }
+
+    virtual double getMSE() const = 0;
+
+};
