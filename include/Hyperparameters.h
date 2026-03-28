d96af9f642a2d2ddcd3dee6e1f68da754bd34631
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2026-03-28 13:17:43 +0200

    made MLmodel header

--- include/Hyperparameters.h
+++ include/Hyperparameters.h
@@ -4,7 +4,8 @@ class Hyperparameters{
     int inputFeatures;
     double learningRate;
     int epochs;
-
+    
+public:
     Hyperparameters(int feat = 1, double lr = 0.001, int eps = 10) 
         : inputFeatures(feat), learningRate(lr), epochs(eps) {}
 };
---
+++ include/MLModel.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+#include "Hyperparameters.h"
+#include "Dataset.h"
+
+#include <Eigen/Dense>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann :: json;
+
+
+class MLModel{
+    std :: string name;
+    bool isTrained;
+    Hyperparameters params;
+
+public:
+    MLModel() {}
+    MLModel(std::string modelName, const Hyperparameters& hp)
+        : name(modelName), isTrained(false), params(hp) {}
+
+    virtual ~MLModel() = default;
+
+    // methods
+    virtual void train(const Dataset& data) = 0;
+    virtual double predict(const Eigen :: VectorXd& input) = 0;
+
+    // save / load to json
+    virtual json serialize() const = 0;
+    virtual void deserialize(const json& j) = 0;
+
+    // getters / setters
+    std :: string getName() const{
+        return name;
+    }
+
+    bool getIsTrained() const{
+        return isTrained;
+    }
+
+};
