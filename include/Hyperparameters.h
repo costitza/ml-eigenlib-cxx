5a590cf3f2f050a03810a64029fce9f9da63d56b
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2026-03-28 12:42:31 +0200

    started making structure for project

---
+++ include/Hyperparameters.h
@@ -0,0 +1,10 @@
+#pragma once 
+
+class Hyperparameters{
+    int inputFeatures;
+    double learningRate;
+    int epochs;
+
+    Hyperparameters(int feat = 1, double lr = 0.001, int eps = 10) 
+        : inputFeatures(feat), learningRate(lr), epochs(eps) {}
+};
