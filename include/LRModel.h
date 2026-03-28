1122c4cae41b876d9459ade4c9382f93644b03c1
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2026-03-28 15:23:01 +0200

    model purely derived from regressor

---
+++ include/LRModel.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Regressor.h"
+#include <Eigen/Dense>
+
+// model for linear regression derived from Regressor
+// the predict method will return the dot product of the input and the weigths
+// the train method will use the normal equation to calculate the weights
+
+// the normal equation is given by: w = (X^T * X + l2Penalty * I)^(-1) * X^T * y
+// where X is the input data, y is the target values, l2Penalty is the regularization term and I is the identity matrix
+
+// eigen vector Xd will be used to store the weights and the bias will be stored in a separate variable
+
+
+class LRModel : public Regressor{
+    Eigen :: VectorXd weights;
+    double bias;
+
+public:
+    LRModel() : Regressor(), bias(0.0) {}
+    LRModel(std::string modelName, const Hyperparameters& hp, double l2 = 0.0, double b = 0.0)
+        : Regressor(modelName, hp, l2), bias(b) {}
+
+    virtual ~LRModel() = default;
+
+    // methods
+    void train(const Dataset& data) override;
+    double predict(const Eigen :: VectorXd& input) override;
+
+    // save / load to json
+    json serialize() const override;
+    void deserialize(const json& j) override;
+
+    double getMSE() const override;
+
+    // getters / setters
+    Eigen :: VectorXd getWeights() const{
+        return weights;
+    }
+
+    double getBias() const{
+        return bias;
+    }
+
+};
